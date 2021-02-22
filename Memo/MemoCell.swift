fcbaa3370e9fdf4ba4f1aebed1ad3cf57ddb17a7
천수빈 <suebin710@gmail.com>
2021-02-22 20:36:29 +0900

    add controller files

---
+++ Memo/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  Memo
+//
+//  Created by exception on 2021/02/22.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Memo/MemoFormVC.swift
@@ -0,0 +1,29 @@
+//
+//  MemoFormVC.swift
+//  Memo
+//
+//  Created by exception on 2021/02/22.
+//
+
+import UIKit
+
+class MemoFormVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
