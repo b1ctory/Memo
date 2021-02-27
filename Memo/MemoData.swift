78e5b600f6e5a79dca9e3d051f297ee809ff3a51
천수빈 <suebin710@gmail.com>
2021-02-27 20:32:34 +0900

    add MemoData

---
+++ Memo/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  Memo
+//
+//  Created by exception on 2021/02/27.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int? // 데이터 식별값
+    var title : String? // 메모 제목
+    var contents : String? // 메모 내용
+    var image : UIImage? // 이미지
+    var regdate : Date? // 작성일
+}
